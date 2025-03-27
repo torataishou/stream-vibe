07e95f41f10af8a4772d10c56a2652f6e471a00b
torataishou <torataishou@zohomail.com>
2025-03-28 02:50:43 +0500

    Configured postcssPxToRem

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+import postcssPxToRem from 'postcss-pxtorem';
+
+export default ({env}) => {
+    const isProd = env === 'production';
+    const plugins = [];
+    if (isProd) {
+        plugins.push(postcssPxToRem({
+                propList: ['*'],
+                mediaQuery: true,
+
+            })
+        );
+    }
+    return {
+        plugins,
+    };
+}
