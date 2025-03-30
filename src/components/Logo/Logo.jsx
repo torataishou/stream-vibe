fa8fbfd3e970352608ae3d27f9971193fef5d206
torataishou <torataishou@zohomail.com>
2025-03-30 16:29:40 +0500

    Added styles for logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,22 @@
+import './Logo.scss';
+import classNames from 'classnames';
+
+const Logo = (props) => {
+    const {
+        className = '',
+        loading = 'lazy',
+    } = props;
+    const title = 'Home';
+
+    return (
+        <a className={classNames(className, 'logo')}
+           href="/"
+           title={title}
+           aria-label={title}
+        >
+            <img className="logo__image" src="/Logo.svg" alt="" width={199} height={60} loading={loading}/>
+        </a>
+    );
+};
+
+export default Logo;
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,13 +1,17 @@
 import './Header.scss';
+import Logo from '@/components/Logo';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header__inner container">
-                
+                <Logo
+                    className="haeder__logo"
+                    loading="eager"
+                />
             </div>
         </header>
+
     );
 };
-
 export default Header;
