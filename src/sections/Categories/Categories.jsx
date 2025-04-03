8809743ba3a8d1c882d2fc517ee1f616c62e7eee
torataishou <torataishou@zohomail.com>
2025-04-04 01:20:19 +0500

    Added slider & slider navigation

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,5 +1,7 @@
 import Section from '@/layouts/Section';
 import CategoryCard from '@/components/CategoryCard';
+import Slider from '@/components/Slider';
+import SliderNavigation from '@/components/Slider/components/SliderNavigation';
 
 const Categories = () => {
     const categoryItems = [
@@ -51,25 +53,32 @@ const Categories = () => {
 
     ]
 
+    const sliderNavigationId = 'categories-slider-navigation'
+
  return (
      <Section
      title="Explore our wide variety of categories"
      titleId="categories-title"
      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
      actions={(
-         <div>
-             <button>Back</button>
-             <button>Forward</button>
-         </div>
+         <SliderNavigation
+             mode="tile"
+             id={sliderNavigationId}
+         />
+
      )}
      isActionsHiddenOnMobile
      >
-         {categoryItems.map((categoryItem, index) => (
-             <CategoryCard
-                 {...categoryItem}
-                 key={index}
-             />
-         ))}
+      <Slider
+        navigationTargetElementId={sliderNavigationId}
+      >
+          {categoryItems.map((categoryItem, index) => (
+              <CategoryCard
+                  {...categoryItem}
+                  key={index}
+              />
+          ))}
+      </Slider>
      </Section>
  )
 }
