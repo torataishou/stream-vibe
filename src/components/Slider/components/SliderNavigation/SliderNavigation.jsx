0c9996eb05d7d023624d041a840610f67a9b8ef8
torataishou <torataishou@zohomail.com>
2025-04-04 16:54:27 +0500

    Configured slider & swiper

--- src/components/Slider/components/SliderNavigation/SliderNavigation.jsx
+++ src/components/Slider/components/SliderNavigation/SliderNavigation.jsx
@@ -20,6 +20,7 @@ const SliderNavigation = (props) => {
             [`slider-navigation--${mode}`]: mode,
         })}
         id={id}
+        data-js-slider-navigation=""
         >
             <Button
                 className="slider-navigation__arrow-button slider-navigation__arrow-button--previous"
@@ -27,16 +28,25 @@ const SliderNavigation = (props) => {
                 iconName="arrow-left"
                 label="Previous slide"
                 isLabelHidden
+                extraAttrs={{
+                    'data-js-slider-previous-button': '',
+                }}
             />
             {hasPagination && (
-                <div className="slider-navigation__pagination"/>
-            )}
+                <div
+                    className="slider-navigation__pagination"
+                    data-js-slider-pagination=""
+                />
+                )}
             <Button
                 className="slider-navigation__arrow-button slider-navigation__arrow-button--next"
                 mode="black-10"
                 iconName="arrow-right"
                 label="Next slide"
                 isLabelHidden
+                extraAttrs={{
+                    'data-js-slider-next-button': '',
+                }}
             />
         </div>
     )
