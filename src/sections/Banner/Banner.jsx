0a53ef7ff8ef2ec2b3a53f4a4bd47297bd833d9b
torataishou <torataishou@zohomail.com>
2025-04-09 02:05:16 +0500

    Added banner

---
+++ src/sections/Banner/Banner.jsx
@@ -0,0 +1,35 @@
+import './Banner.scss'
+import Button from '@/components/Button';
+
+const Banner = (props) => {
+    const titleId = 'banner-title'
+
+    const {
+
+    } = props
+
+    return (
+      <section
+      className="banner container"
+      aria-labelledby={titleId}
+      >
+        <div className="banner__inner">
+            <div className="banner__body">
+                <h2 className="banner__title" id={titleId}>
+                    Start your free trial today!
+                </h2>
+                <div className="banner__description">
+                    <p>This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe.</p>
+                </div>
+            </div>
+            <Button
+            className="banner__button"
+            label="Start a Free Trail"
+            href="/subscribtions"
+            />
+        </div>
+      </section>
+    );
+};
+
+export default Banner;
