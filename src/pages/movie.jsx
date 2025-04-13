6795a933843eb58887341d4b8db5e628eac7d5ec
torataishou <torataishou@zohomail.com>
2025-04-14 04:00:45 +0500

    Added section MovieBanner

---
+++ src/pages/movie.jsx
@@ -0,0 +1,14 @@
+import MovieBanner from '@/sections/MovieBanner';
+
+
+export const metadata = {
+    title: 'Movie - Kantara',
+}
+
+export default function () {
+    return (
+        <>
+          <MovieBanner/>
+        </>
+    )
+}
